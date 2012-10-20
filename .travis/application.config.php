d57e7ce04a76e33a8e21610b50009980cfc31b27
Michaël Gallego <mic.gallego@gmail.com>
2012-10-20 17:34:45 +0200

    Test travis

---
+++ .travis/application.config.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+    'modules' => array(
+        'Application',
+        'ZfrForum',
+    ),
+    'module_listener_options' => array(
+        'config_glob_paths'    => array(
+            'config/autoload/{,*.}{global,local}.php',
+        ),
+        'module_paths' => array(
+            './module',
+            './vendor',
+        ),
+    ),
+    'service_manager' => array(
+        'use_defaults' => true,
+        'factories'    => array(
+        ),
+    ),
+);
