9a17bbca33638a9cab88b761ea5e1201a0329c5f
Michaël Gallego <mic.gallego@gmail.com>
2012-11-23 22:38:54 +0100

    Tests Angular

--- public/js/angular.js
+++ public/js/angular.js
@@ -1,17 +1,30 @@
-angular.module('postServices', ['ngResource']).
-    factory('Post', function($resource) {
-        alert('ok');
-        return $resource('/posts/:postId', {}, {
-            query: {method: 'GET', params: {postId: 'post'}, isArray:true}
+angular.module('threadServices', ['ngResource']).
+    factory('Thread', function($resource) {
+        return $resource('/forum/threads/:id', {}, {
+            query: {method: 'GET', params: {id: 'thread'}, isArray: true}
         });
     });
 
-angular.module('post', ['postServices']).
-    config(['$routeProvider', function($routeProvider) {
-        $routeProvider.
-            when('/posts/:postId', {templateUrl: 'partials/post-detail', controller: PostDetailController})
-}]);
+angular.module('postServices', ['ngResource']).
+    factory('Post', function($resource) {
+        return $resource('/forum/threads/:threadId/posts', {}, {
+            query: {method: 'GET', isArray: true}
+        });
+    });
 
-function PostDetailController($scope, $routeParams, Post) {
-    alert('ok');
+function ThreadDetailController($scope, $routeParams, Thread) {
+    $scope.thread = Thread.get({id: $routeParams.id});
 }
+
+function PostsController($scope, $routeParams, Post) {
+    $scope.posts = Post.get({threadId: $routeParams.id});
+}
+
+angular.module('app', ['threadServices', 'postServices']).
+    config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.
+        when('/forum/threads/:id', {templateUrl: '/partials/thread-detail.html', controller: ThreadDetailController}).
+        when('/forum/threads/:threadId/messages', {templateUrl: '/partials/messages.html', controller: PostsController})
+}]);
